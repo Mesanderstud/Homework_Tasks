edbc33d9b0610bbb1a3713f77d421060468667de
Mesanderstud <146775654+Mesanderstud@users.noreply.github.com>
2023-10-19 17:37:54 +0500

    Add files via upload

---
+++ ClassDoctor_Homework/ClassDoctor_Homework/Program.cs
@@ -0,0 +1,74 @@
+﻿abstract class Doctor
+{
+    public abstract void TreatPatient(Patient patient);
+}
+
+class Surgeon : Doctor
+{
+    public override void TreatPatient(Patient patient)
+    {
+        Console.WriteLine("Хирург лечит пациента " + patient.Name);
+    }
+}
+
+class Therapist : Doctor
+{
+    public override void TreatPatient(Patient patient)
+    {
+        Console.WriteLine("Терапевт лечит пациента " + patient.Name);
+    }
+}
+
+class Dentist : Doctor
+{
+    public override void TreatPatient(Patient patient)
+    {
+        Console.WriteLine("Дантист лечит пациента " + patient.Name);
+    }
+}
+
+class Patient
+{
+    public string Name { get; set; }
+    public TreatmentPlan Plan { get; set; }
+}
+
+class TreatmentPlan
+{
+    public int TreatCode { get; set; }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Doctor surgeon = new Surgeon();
+        Doctor therapist = new Therapist();
+        Doctor dentist = new Dentist();
+
+        Patient patient = new Patient()
+        {
+            Name = "Ноунейм",
+            Plan = new TreatmentPlan() { TreatCode = 1 }
+        };
+
+        Console.WriteLine("Выберите у какого доктора вы хотите лечиться? (1 - Хирург, 2 - Терапевт, 3 - Дантист)");
+        int choice = int.Parse(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            surgeon.TreatPatient(patient);
+        }
+        else if (choice == 2)
+        {
+            therapist.TreatPatient(patient);
+        }
+        else if (choice == 3)
+        {
+            dentist.TreatPatient(patient);
+        }
+        else
+        {
+            Console.WriteLine("Неверный номер доктора у которого хотите лечится.");
+        }
+    }
+}
---
+++ ClassPhone_Homework/ClassPhone_Homework/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace PhoneCall
+{
+    class Phone
+    {
+        private string number;
+        private string model;
+        private string weight;
+
+        public string Number { get { return number; } }
+        public string Model { get { return model; } }
+        public string Weight { get { return weight; } }
+
+        public Phone(string number, string model, string weight)
+        {
+            this.number = number;
+            this.model = model;
+            this.weight = weight;
+        }
+
+        public void ReceiveCall(string name)
+        {
+            Console.WriteLine("Звонит " + name);
+        }
+
+        public void SendMessage(params string[] phoneNumbers)
+        {
+            Console.WriteLine("Отправлены сообщения на следующие номера телефонов:");
+            foreach (string number in phoneNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone("8913789239", "iPhone", "150 г");
+            Phone phone2 = new Phone("1382349892", "Samsung", "200 г");
+            Phone phone3 = new Phone("+79812376424", "Nokia", "100 г");
+
+            Console.WriteLine($"Номер 1 телефона: " + phone1.Number + " Модель: " +  phone1.Model + " Вес: " +  phone1.Weight);
+            Console.WriteLine($"Номер 2 телефона: " + phone2.Number + " Модель: " +  phone2.Model + " Вес: " +  phone2.Weight);
+            Console.WriteLine($"Номер 3 телефона: " + phone3.Number + " Модель: " +  phone3.Model + " Вес: " +  phone3.Weight);
+
+            phone1.ReceiveCall("Лешка");
+            Console.WriteLine("Номер телефона: " + phone1.Number);
+
+            phone2.ReceiveCall("Алеша");
+            Console.WriteLine("Номер телефона: " + phone2.Number);
+
+            phone3.ReceiveCall("Алексей");
+            Console.WriteLine("Номер телефона: " + phone3.Number);
+
+            phone1.SendMessage("8913789239", "1382349892");
+            phone2.SendMessage("8913789239", "1382349892", "+79812376424");
+            }
+    }
+}
