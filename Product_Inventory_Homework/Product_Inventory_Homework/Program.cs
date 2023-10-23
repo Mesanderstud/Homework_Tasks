f7d2684eccf5afc5673fca7c1c00bc3a4e93ba0c
Mesanderstud <146775654+Mesanderstud@users.noreply.github.com>
2023-10-23 17:42:27 +0500

    Add files via upload

---
+++ Product_Inventory_Homework/Product_Inventory_Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Product_Inventory_Homework;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Inventory inventory = new Inventory();
+
+       
+        List<Product> productList = new List<Product>
+            {
+                new Product(41300,"Sony", 26),
+                new Product(34600,"LG", 15),
+                new Product(23500,"Dexp", 44)
+            };
+
+        
+        foreach (var product in productList)
+        {
+            inventory.AddProduct(product);
+        }
+
+        inventory.PrintInventory();
+
+        decimal totalValue = inventory.CalculateInventoryValue();
+
+        Console.WriteLine($"Общая стоимость инвентаря: {totalValue}");
+
+        Console.ReadLine();
+    }
+}
